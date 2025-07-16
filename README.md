0c36e7b577546f9fb586b6db03f9c5a9aec0823e
peejaykoy12345 <rick.well.rick@gmail.com>
2025-07-16 19:10:59 +0800

    Finished the project
    - Added print all task function
    - Added add task function
    - Added a main loop

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Todo-list-in-C
 My first C program
+
+# This is my first and last time writing C
--- app.c
+++ app.c
@@ -19,8 +19,7 @@ int findTask(struct Task tasks[], int ID, int task_counter)
 
     while (left <= right)
     {
-        int mid = (int) ((left + right) / 2);
-
+        int mid = (left + right) / 2;
         int task_id = tasks[mid].id;
 
         if (task_id == ID)
@@ -43,7 +42,7 @@ void addTask(struct Task tasks[], int* task_counter, int ID, char desc[MAX_LEN])
 {
     if (*task_counter >= MAX_TASKS)
     {
-        printf("TASKS ARE FULL PLEASE DELETE ONE!");
+        printf("TASKS ARE FULL PLEASE DELETE ONE!\n");
         return;
     }
 
@@ -51,21 +50,89 @@ void addTask(struct Task tasks[], int* task_counter, int ID, char desc[MAX_LEN])
     strncpy(tasks[*task_counter].description, desc, MAX_LEN - 1);
     tasks[*task_counter].description[MAX_LEN - 1] = '\0';
     tasks[*task_counter].completed = false;
+
+    (*task_counter)++; // fix this (was postfix, didn't actually increase the value)
+    printf("Task added!\n");
 }
 
-void deleteTask(struct Task tasks[], int *n, int ID) {
-  
-  	int i = findTask(tasks, ID, *n);
-  	
-    for (int j = i; j < *n - 1; j++) {
+void deleteTask(struct Task tasks[], int *n, int ID)
+{
+    int i = findTask(tasks, ID, *n);
+    if (i == -1)
+    {
+        printf("Task with ID %d not found.\n", ID);
+        return;
+    }
+
+    for (int j = i; j < *n - 1; j++)
+    {
         tasks[j] = tasks[j + 1];
     }
-    (*n)--; 
+    (*n)--;
+    printf("Task deleted!\n");
+}
+
+void printAllTasks(struct Task tasks[], int* task_counter)
+{
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    if (*task_counter == 0)
+    {
+        printf("No tasks yet.\n");
+        return;
+    }
+
+    for (int i = 0; i < *task_counter; i++)
+    {
+        printf("%d. Task Description: %s | Task ID: %d\n", i + 1, tasks[i].description, tasks[i].id);
+    }
+    printf("DONE\n");
 }
 
 int main()
 {
     struct Task tasks[MAX_TASKS] = {};
+    int task_counter = 0;
 
+    while (true)
+    {
+        printf("Input a mode: ");
+        char mode[6];
+        scanf("%5s", mode);
+
+        if (strcmp(mode, "add") == 0)
+        {
+            int id;
+            char desc[MAX_LEN];
+            printf("Enter task ID: ");
+            scanf("%d", &id);
+            getchar(); 
+
+            printf("Enter task description: ");
+            fgets(desc, MAX_LEN, stdin);
+            desc[strcspn(desc, "\n")] = '\0'; 
+
+            addTask(tasks, &task_counter, id, desc);
+        }
+        else if (strcmp(mode, "delete") == 0)
+        {
+            int id;
+            printf("Enter task ID to delete: ");
+            scanf("%d", &id);
+            deleteTask(tasks, &task_counter, id);
+        }
+        else if (strcmp(mode, "print") == 0)
+        {
+            printAllTasks(tasks, &task_counter);
+        }
+        else if (strcmp(mode, "exit") == 0)
+        {
+            break;
+        }
+        else
+        {
+            printf("Invalid input\n");
+        }
+    }
     return 0;
 }
